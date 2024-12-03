aa32a4cc9909fd94f0dbd713fa1a06c7e3bf5432
장준석 <angec5689@naver.com>
2024-12-03 20:42:21 +0900

    Title: 마법의 숲 탐색, Time: 276ms, Memory: 22MB, Status: Passed - CodeTree

--- 241203/마법의 숲 탐색/magical-forest-exploration.java
+++ 241203/마법의 숲 탐색/magical-forest-exploration.java
@@ -1,347 +1,108 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
+    private static final int MAX_L = 70;
 
-    static int R;
-    static int C;
-    static int K;
-    static int[][] map;
-    static Golem[] golems;
+    private static int R, C, K; // 행, 열, 골렘의 개수를 의미합니다
+    private static int[][] A = new int[MAX_L + 3][MAX_L]; // 실제 숲을 [3~R+2][0~C-1]로 사용하기위해 행은 3만큼의 크기를 더 갖습니다
+    private static int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
+    private static boolean[][] isExit = new boolean[MAX_L + 3][MAX_L]; // 해당 칸이 골렘의 출구인지 저장합니다
+    private static int answer = 0; // 각 정령들이 도달할 수 있는 최하단 행의 총합을 저장합니다
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
-        golems = new Golem[K];
-        int answer = 0;
+    // (y, x)가 숲의 범위 안에 있는지 확인하는 함수입니다
+    private static boolean inRange(int y, int x) {
+        return 3 <= y && y < R + 3 && 0 <= x && x < C;
+    }
 
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int c = Integer.parseInt(st.nextToken()) - 1;
-            int exit = Integer.parseInt(st.nextToken());
-            golems[i] = new Golem(c, exit, i + 1);
-        }
-
-        //정령 내리기
-        /* 정령 이동 우선순위
-            1. 남쪽으로만
-            2. 서쪽 후 남 이때 방향 (d - 1) % 4
-            3. 동쪽 후 남 이때 방향 (d + 1) % 4
-        */
-        for (int i = 0; i < K; i++) {
-            Golem golem = golems[i];
-
-            while (true) {
-                if (golem.isMoveSouth(R, C, map)) {
-                    golem.goSouth(map);
-                } else if (golem.isMoveWestAndSouth(R, C, map)) {
-                    golem.goWest(map);
-                    golem.goSouth(map);
-                } else if (golem.isMoveEastAndSouth(R, C, map)) {
-                    golem.goEast(map);
-                    golem.goSouth(map);
-                } else break;
-            }
-            // for(int j = 0; j < R; j++) {
-            //     System.out.println(Arrays.toString(map[j]));
-            // }System.out.println();
-            if (golem.isOutBody(R, C)) {
-                map = new int[R][C];
-            } else {
-                // System.out.println(golem.getLastRowNumber(R, C, map));
-                // System.out.println("--------------\n");
-                answer += golem.getLastRowNumber(R, C, map);
+    // 숲에 있는 골렘들이 모두 빠져나갑니다
+    private static void resetMap() {
+        for (int i = 0; i < R + 3; i++) {
+            for (int j = 0; j < C; j++) {
+                A[i][j] = 0;
+                isExit[i][j] = false;
             }
         }
-        System.out.println(answer);
-    }
-}
-
-class Golem {
-    Point p; //골렘 중앙 좌표
-    int exit; //골렘 출구 방향
-    int number; //골렘의 번호
-
-    public Golem(int c, int exit, int number) {
-        this.p = new Point(-2, c);
-        this.exit = exit;
-        this.number = number;
-    }
-    
-    public int getR() {
-        return this.p.r;
     }
 
-    public int getC() {
-        return this.p.c;
+    // 골렘의 중심이 y, x에 위치할 수 있는지 확인합니다.
+    // 북쪽에서 남쪽으로 내려와야하므로 중심이 (y, x)에 위치할때의 범위와 (y-1, x)에 위치할떄의 범위 모두 확인합니다
+    private static boolean canGo(int y, int x) {
+        boolean flag = 0 <= x - 1 && x + 1 < C && y + 1 < R + 3;
+        flag = flag && (A[y - 1][x - 1] == 0);
+        flag = flag && (A[y - 1][x] == 0);
+        flag = flag && (A[y - 1][x + 1] == 0);
+        flag = flag && (A[y][x - 1] == 0);
+        flag = flag && (A[y][x] == 0);
+        flag = flag && (A[y][x + 1] == 0);
+        flag = flag && (A[y + 1][x] == 0);
+        return flag;
     }
 
-    public Point getExitPoint() {
-        switch (this.exit) {
-            case 0:
-            return new Point(this.getR() - 1, this.getC());
-
-            case 1:
-            return new Point(this.getR(), this.getC() + 1);
-            
-            case 2:
-            return new Point(this.getR() + 1, this.getC());
-            
-            case 3:
-            return new Point(this.getR(), this.getC() - 1);
-        }
-        return null;
-    }
-
-    public boolean isOutBody(int R, int C) {
-        int topR = this.getR() - 1;
-        return topR < 0;
-    }
-
-    public int getLastRowNumber(int R, int C, int[][] map) {
-        Deque<Unit> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[R][C];
-        int[][] d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        q.add(new Unit(this.getR(), this.getC(), this.number));
-        visited[this.getR()][this.getC()] = true;
-        int maxRowNumber = Integer.MIN_VALUE;
-
+    // 정령이 움직일 수 있는 모든 범위를 확인하고 도달할 수 있는 최하단 행을 반환합니다
+    private static int bfs(int y, int x) {
+        int result = y;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visit = new boolean[MAX_L + 3][MAX_L];
+        q.offer(new int[]{y, x});
+        visit[y][x] = true;
         while (!q.isEmpty()) {
-            Unit unit = q.poll();
-
-            maxRowNumber = Math.max(maxRowNumber, unit.r + 1);
-
-            for (int i = 0; i < 4; i++) {
-                int tr = unit.r + d[i][0];
-                int tc = unit.c + d[i][1];
-
-                if (tr < 0 || tr >= R || tc < 0 || tc >= C || visited[tr][tc]) continue;
-                if (map[tr][tc] == unit.golemNumber) {
-                    visited[tr][tc] = true;
-                    q.add(new Unit(tr, tc, unit.golemNumber));
-                } else if (map[tr][tc] < 0 && map[tr][tc] * -1 == unit.golemNumber) {
-                    visited[tr][tc] = true;
-                    q.add(new Unit(tr, tc, map[tr][tc]));
-                } else if (unit.golemNumber < 0 && map[tr][tc] != 0) {
-                    visited[tr][tc] = true;
-                    q.add(new Unit(tr, tc, map[tr][tc]));
+            int[] cur = q.poll();
+            for (int k = 0; k < 4; k++) {
+                int ny = cur[0] + dy[k], nx = cur[1] + dx[k];
+                // 정령의 움직임은 골렘 내부이거나
+                // 골렘의 탈출구에 위치하고 있다면 다른 골렘으로 옮겨 갈 수 있습니다
+                if (inRange(ny, nx) && !visit[ny][nx] && (A[ny][nx] == A[cur[0]][cur[1]] || (A[ny][nx] != 0 && isExit[cur[0]][cur[1]]))) {
+                    q.offer(new int[]{ny, nx});
+                    visit[ny][nx] = true;
+                    result = Math.max(result, ny);
                 }
             }
         }
-        return maxRowNumber;
+        return result;
     }
 
-    //이동 전 흔적 지우기
-    public void removeGolemNumber(int[][] map) {
-        int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (int i = 0; i < 5; i++) {
-            int tr = this.getR() + d[i][0];
-            int tc = this.getC() + d[i][1];
-            if (tr < 0) continue;
-            map[tr][tc] = 0;
-        }
-    }
-
-    public boolean isMoveWestAndSouth(int R, int C, int[][] map) {
-        if (!this.isMoveWest(R, C, map)) return false;
-        Point tmpP = this.p;
-        int exitTmp = this.exit;
-
-        this.goWest(map);
-        boolean isMoveSouthFlag = this.isMoveSouth(R, C, map);
-
-        this.removeGolemNumber(map);
-        this.p = tmpP;
-        this.exit = exitTmp;
-        int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (int i = 0; i < 5; i++) {
-            int tr = this.getR() + d[i][0];
-            int tc = this.getC() + d[i][1];
-            if (tr < 0) continue;
-            map[tr][tc] = this.number;
-        }
-        Point exitP = this.getExitPoint();
-        if (exitP.r >= 0) map[exitP.r][exitP.c] = this.number * -1;
-
-        return isMoveSouthFlag;
-    }
-
-    public boolean isMoveEastAndSouth(int R, int C, int[][] map) {
-        if (!this.isMoveEast(R, C, map)) return false;
-        Point tmpP = this.p;
-        int exitTmp = this.exit;
-
-        this.goEast(map);
-        boolean isMoveSountFlag = this.isMoveSouth(R, C, map);
-
-        this.removeGolemNumber(map);
-        this.p = tmpP;
-        this.exit = exitTmp;
-        int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (int i = 0; i < 5; i++) {
-            int tr = this.getR() + d[i][0];
-            int tc = this.getC() + d[i][1];
-            if (tr < 0) continue;
-            map[tr][tc] = this.number;
-        }
-        Point exitP = this.getExitPoint();
-        if (exitP.r >= 0) map[exitP.r][exitP.c] = this.number * -1;
-
-        return isMoveSountFlag;
-    }
-
-    public boolean isMoveSouth(int R, int C, int[][] map) {
-        //왼쪽, 오른쪽, 아래 이동 후포인트 좌표
-        Point[] points = new Point[3];
-        points[0] = new Point(this.getR() + 1, this.getC() - 1);
-        points[1] = new Point(this.getR() + 1, this.getC() + 1);
-        points[2] = new Point(this.getR() + 2, this.getC());
-
-        boolean rangeFlag = true;
-        boolean willMoveFlag = true;
-
-        //범위를 충족하는지
-        rangeFlag &= (points[2].r < R);
-        if (!rangeFlag) return rangeFlag;
-
-        //남쪽으로 갈 수 있는지
-        for (int i = 0; i < 3; i++) {
-            if (points[i].r < 0) continue;
-            int mapNumber = map[points[i].r][points[i].c];
-            if (mapNumber != 0 && mapNumber != this.number) {
-                willMoveFlag = false;
-                break;
+    // 골렘id가 중심 (y, x), 출구의 방향이 d일때 규칙에 따라 움직임을 취하는 함수입니다
+    // 1. 남쪽으로 한 칸 내려갑니다.
+    // 2. (1)의 방법으로 이동할 수 없으면 서쪽 방향으로 회전하면서 내려갑니다.
+    // 3. (1)과 (2)의 방법으로 이동할 수 없으면 동쪽 방향으로 회전하면서 내려갑니다.
+    private static void down(int y, int x, int d, int id) {
+        if (canGo(y + 1, x)) {
+            // 아래로 내려갈 수 있는 경우입니다
+            down(y + 1, x, d, id);
+        } else if (canGo(y + 1, x - 1)) {
+            // 왼쪽 아래로 내려갈 수 있는 경우입니다
+            down(y + 1, x - 1, (d + 3) % 4, id);
+        } else if (canGo(y + 1, x + 1)) {
+            // 오른쪽 아래로 내려갈 수 있는 경우입니다
+            down(y + 1, x + 1, (d + 1) % 4, id);
+        } else {
+            // 1, 2, 3의 움직임을 모두 취할 수 없을떄 입니다.
+            if (!inRange(y-1, x-1) || !inRange(y+1, x+1)) {
+                // 숲을 벗어나는 경우 모든 골렘이 숲을 빠져나갑니다
+                resetMap();
+            } else {
+                // 골렘이 숲 안에 정착합니다
+                A[y][x] = id;
+                for (int k = 0; k < 4; k++)
+                    A[y + dy[k]][x + dx[k]] = id;
+                // 골렘의 출구를 기록하고
+                isExit[y + dy[d]][x + dx[d]] = true;
+                // bfs를 통해 정령이 최대로 내려갈 수 있는 행를 계산하여 누적합합니다
+                answer += bfs(y, x) - 3 + 1;
             }
         }
-        return willMoveFlag;
     }
 
-    public void goSouth(int[][] map) {
-        removeGolemNumber(map);
-        this.p = new Point(this.getR() + 1, this.getC());
-        int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (int i = 0; i < 5; i++) {
-            int tr = this.getR() + d[i][0];
-            int tc = this.getC() + d[i][1];
-            if (tr < 0) continue;
-            map[tr][tc] = this.number;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        R = scanner.nextInt();
+        C = scanner.nextInt();
+        K = scanner.nextInt();
+        for (int id = 1; id <= K; id++) { // 골렘 번호 id
+            int x = scanner.nextInt() - 1;
+            int d = scanner.nextInt();
+            down(0, x, d, id);
         }
-
-        Point exitP = this.getExitPoint();
-        if (exitP.r >= 0) map[exitP.r][exitP.c] = this.number * -1;
-    }
-
-    public boolean isMoveWest(int R, int C, int[][] map) {
-        //왼쪽, 위, 아래 이동 후포인트 좌표
-        Point[] points = new Point[3];
-        points[0] = new Point(this.getR(), this.getC() - 2);
-        points[1] = new Point(this.getR() - 1, this.getC() - 1);
-        points[2] = new Point(this.getR() + 1, this.getC() - 1);
-
-        boolean rangeFlag = true;
-        boolean willMoveFlag = true;
-
-        //범위를 충족하는지
-        rangeFlag &= (points[0].c >= 0);
-        if (!rangeFlag) return rangeFlag;
-        
-        //서쪽으로 갈 수 있는지
-        for (int i = 0; i < 3; i++) {
-            if (points[i].r < 0) continue;
-            int mapNumber = map[points[i].r][points[i].c];
-            if (mapNumber != 0 && mapNumber != this.number) {
-                willMoveFlag = false;
-                break;
-            }
-        }
-        return willMoveFlag;
-    }
-
-    //서쪽 이동
-    public void goWest(int[][] map) {
-        this.exit = (this.exit - 1 + 4) % 4;
-
-        removeGolemNumber(map);
-        this.p = new Point(this.getR(), this.getC() - 1);
-        int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (int i = 0; i < 5; i++) {
-            int tr = this.getR() + d[i][0];
-            int tc = this.getC() + d[i][1];
-            if (tr < 0) continue;
-            map[tr][tc] = this.number;
-        }
-
-        Point exitP = this.getExitPoint();
-        if (exitP.r >= 0) map[exitP.r][exitP.c] = this.number * -1;
-    }
-
-    public boolean isMoveEast(int R, int C, int[][] map) {
-        //오른쪽, 위, 아래 이동 후포인트 좌표
-        Point[] points = new Point[3];
-        points[0] = new Point(this.getR(), this.getC() + 2);
-        points[1] = new Point(this.getR() - 1, this.getC() + 1);
-        points[2] = new Point(this.getR() + 1, this.getC() + 1);
-
-        boolean rangeFlag = true;
-        boolean willMoveFlag = true;
-
-        //범위를 충족하는지
-        rangeFlag &= (points[0].c < C);
-        if (!rangeFlag) return rangeFlag;
-
-        //동쪽으로 갈 수 있는지
-        for (int i = 0; i < 3; i++) {
-            if (points[i].r < 0) continue;
-            int mapNumber = map[points[i].r][points[i].c];
-            if (mapNumber != 0 && mapNumber != this.number) {
-                willMoveFlag = false;
-                break;
-            }
-        }
-        return willMoveFlag;
-    }
-
-    //동쪽 이동
-    public void goEast(int[][] map) {
-        this.exit = (this.exit + 1 + 4) % 4;
-
-        removeGolemNumber(map);
-        this.p = new Point(this.getR(), this.getC() + 1);
-        int[][] d = {{0, 0}, {-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        for (int i = 0; i < 5; i++) {
-            int tr = this.getR() + d[i][0];
-            int tc = this.getC() + d[i][1];
-            if (tr < 0) continue;
-            map[tr][tc] = this.number;
-        }
-
-        Point exitP = this.getExitPoint();
-        if (exitP.r >= 0) map[exitP.r][exitP.c] = this.number * -1;
-    }
-}
-
-class Unit {
-    int r;
-    int c;
-    int golemNumber;
-
-    public Unit(int r, int c, int golemNumber) {
-        this.r = r;
-        this.c = c;
-        this.golemNumber = golemNumber;
-    }
-}
-
-class Point {
-    int r;
-    int c;
-
-    public Point(int r, int c) {
-        this.r = r;
-        this.c = c;
+        System.out.println(answer);
     }
 }
